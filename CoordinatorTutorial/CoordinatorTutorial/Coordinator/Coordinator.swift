8cf42b50164f2b519fb049e98a790a2366a325e2
Alan Milke <alan.marquez@bairesdev.com>
2022-08-09 14:43:54 -0500

    Changed to StoryBoards and set the Coordinator

--- CoordinatorTutorial/CoordinatorTutorial/Coordinator/Coordinator.swift
+++ CoordinatorTutorial/CoordinatorTutorial/Coordinator/Coordinator.swift
@@ -1,14 +1,7 @@
-//
-//  Coordinator.swift
-//  CoordinatorTutorial (iOS)
-//
-//  Created by Alan Milke on 09/08/22.
-//
-
-import Foundation
 import UIKit
 
 protocol Coordinator {
+    
     var childCoordinators: [Coordinator] { get set }
     var navigationController: UINavigationController { get set }
     
---
+++ CoordinatorTutorial/CoordinatorTutorial/Coordinator/MainCoordinator.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class MainCoordinator: Coordinator {
+    
+    var childCoordinators: [Coordinator] = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = ViewController.instantiate()
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
